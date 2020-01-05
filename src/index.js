86c07f80efab80a1e98390dd1f2a723d4a771081
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-01-04 21:39:41 -0500

    Remove bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
